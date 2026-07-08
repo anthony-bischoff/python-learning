fe9f93bd318ad1a53f8096652a81dfefb0c944c1
Bischoff Anthony <Anthony_Bischoff@protonmail.com>
2026-07-08 14:27:41 +0200

    exo string final

---
+++ wikibullet.py
@@ -0,0 +1,8 @@
+import pyperclip
+text = pyperclip.paste()
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '*' + lines[i]
+pyperclip.copy(text)
+text = '\n'.join(lines)
+pyperclip.copy(text)
